a0fd888da102a9ed7dd152c792ffd06facdea1dc
Strothmann <marvin.wagemann@hs-osnabrueck.de>
2023-07-31 20:15:57 +0200

    App&
    Grundlagen der WebApp und angefangen den M5 Stack als Kamera zu nutzen

--- m5stack/iot_projekt/src/main.cpp
+++ m5stack/iot_projekt/src/main.cpp
@@ -1,11 +1,38 @@
 #include <M5Stack.h>
+#include <WiFi.h>
+#include "HTTPClient.h"
+const char* Adresse = "";
+const char* password = "";
+
 
 //Das ist nur ein Versuch
 
-void setup() {
+void setup() 
+{
   // put your setup code here, to run once:
 }
 
-void loop() {
+void loop() 
+{
   // put your main code here, to run repeatedly:
+}
+
+//Soll den M5 Stack mit dem Wlan verbinden um die Bilddaten zu übertragen
+void connect() 
+{
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    M5.Lcd.print(".");
+  }
+  M5.Lcd.println("");
+  M5.Lcd.println("WiFi connected!");
+  // connect to http server
+  client.begin(String(server_url));
+  client.sendRequest("CONNECT");
+  while (!client.connected()) {
+    delay(500);
+    M5.Lcd.print(".");
+  }
+  M5.Lcd.println("connected to http server");
 }
